253c34c81cd1d392a691ce9c59ed5822815ee3b1
Abdoul Aziz Yatagan <124500030+ABDOUSALAM1942@users.noreply.github.com>
2023-02-18 15:51:59 +0100

    Add files via upload

---
+++ checkSign.js
@@ -0,0 +1,23 @@
+function checkSign(i) {
+    return i > 0;
+}
+
+var i = -6;
+if (i >= 0) {
+    console.log('Positif');
+} else {
+    console.log('Negatif');
+}
+var i = 0;
+if (i >= 0) {
+    console.log('Positif');
+} else {
+    console.log('Negatif');
+}
+var i = 4;
+if (i >= 0) {
+    console.log('Positif');
+} else {
+    console.log('Negatif');
+}
+module.exports = checkSign
---
+++ countOneToHunds.js
@@ -0,0 +1,9 @@
+
+function countOneToHunds(i) {
+    return i;
+}
+for (let i = 1; i <= 100; i++)
+    console.log(i);
+
+
+module.exports = countOneToHunds
