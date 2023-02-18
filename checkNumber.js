86d16c738609a5e7a58f185f2602865cd0364b0c
Abdoul Aziz Yatagan <124500030+ABDOUSALAM1942@users.noreply.github.com>
2023-02-19 00:42:22 +0100

    Add files via upload

---
+++ checkNumber.js
@@ -0,0 +1,14 @@
+function checkNumber(n) {
+    n <= 10;
+    if (n < 5) {
+        return "Faible";
+    }
+    else if (n === 5 || n === 6) {
+        return "Moyen";
+    }
+}
+console.log(checkNumber(2));
+console.log(checkNumber(5));
+console.log(checkNumber(6));
+
+module.exports = checkNumber;
---
+++ checkSign.js
@@ -0,0 +1,13 @@
+function checkSign(n) {
+    if (n < 0) {
+        return "Negatif";
+    }
+    else {
+        return "Positif";
+    }
+}
+console.log(checkSign(-10));
+console.log(checkSign(0));
+console.log(checkSign(10));
+
+module.exports = checkSign;
